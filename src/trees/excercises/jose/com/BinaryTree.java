f3632426683d7f514032b8d3af23d03f5c82b50c
jose.marcial <jose.marcial@mx-it00868.globant.com>
2018-03-22 00:57:40 -0600

    adding

---
+++ src/trees/excercises/jose/com/BinaryTree.java
@@ -0,0 +1,26 @@
+package trees.excercises.jose.com;
+
+public class BinaryTree {
+	
+	
+
+}
+
+ class Node {
+	int data;
+	Node leftNode;
+	Node rigthNode;
+	
+	public Node(int data, Node leftNode, Node rigthNode) {
+		super();
+		this.data = data;
+		this.leftNode = leftNode;
+		this.rigthNode = rigthNode;
+	}
+	
+	public Node(int data) {
+		super();
+		this.data = data;
+		
+	}
+}
